ba503e0295feb056182658dabc24256d740331eb
loressl <loryssl@hotmail.com>
2021-03-15 01:33:31 -0300

    Testes.

--- src/store/storeConfig.js
+++ src/store/storeConfig.js
@@ -1,17 +1,17 @@
-import { 
+import {
     combineReducers,
     createStore,
     compose,
     applyMiddleware
 } from 'redux'
 import thunk from 'redux-thunk'
-import searchReducer from './reducers/search'
+import searchReducer  from './reducers/search'
 
 const reducers = combineReducers({
     search: searchReducer,
 })
 
-const storeConfig = () =>{
+const storeConfig = () => {
     return createStore(reducers, compose(applyMiddleware(thunk)))
 }
 
